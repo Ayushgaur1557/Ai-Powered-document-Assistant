523225729efc7c37397a2b760d94cfe7e186e9d6
Ayush Gaur <ayushgaur061@example.com>
2025-04-15 21:14:22 +0530

    changed in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -87,12 +87,6 @@ function App() {
 
     <div className="min-h-screen bg-gradient-to-b from-gray-100 to-white flex items-center justify-center p-4">
 
-<button
-  onClick={() => navigate('/dashboard')}
-  className="mb-4 bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800"
->
-  🚀 Go to Dashboard
-</button>
 
       <Toaster position="top-right" /> 
 
